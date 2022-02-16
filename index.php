94553b754fa81fd27b0387ebeb01cac77f22ddcc
kiran <test@w3schools.com>
2022-02-16 17:55:48 +0530

    updated index.html with emergency fix

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-THis is new file for git status updation.
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello World!</title>
+<link rel="stylesheet" href="bluestyle.css">
+</head>
+<body>
+
+<h1>Hello world!</h1>
+<p>This is the first file in my new Git Repo.</p>
+<p>This line is here to show how merging works.</p>
+
+</body>
+</html> 
